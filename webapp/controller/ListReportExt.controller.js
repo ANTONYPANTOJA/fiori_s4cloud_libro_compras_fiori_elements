f2f6876586d4fef035ff09fdfdab61620233134e
ANTONYPANTOJA <anthonypantojaari@gmail.com>
2024-11-24 00:38:30 +0000

    Desarrollo

--- webapp/controller/ListReportExt.controller.js
+++ webapp/controller/ListReportExt.controller.js
@@ -19,18 +19,55 @@ function (Controller) {
             }
 
             //model
-            const model = this.getOwnerComponent().getModel();
-            if (model) {
-                
+            let gridTable = this.getView().byId("ns.asa.zappbookpurcharse::sap.suite.ui.generic.template.ListReport.view.ListReport::BookPurcharse--GridTable");
+            if (gridTable) {
+                gridTable.setThreshold(1000000);
+            }
+        },
+        onBeforeRendering: function(){
+
+            try {
+                const postAction = this.byId("postAction");
+                if (postAction) {
+                    postAction.setIcon("sap-icon://excel-attachment");
+                }       
+            } catch (error) {
+                console.error("Error Function: onBeforeRendering ", error);
+            }
+            
+        },
+        postExcel: function(oEvent){
+            const oSource     = oEvent.getSource();
+            const oGridTable  = oSource.getParent().getParent().getTable(); 
+            if (oGridTable) {
+                const model = oGridTable.getModel();
+                if (model) {
+                    if (model.oData) {
+                        
+                    }
+                }
             }
         },
         onBeforeRebindTableExtension: function(oEvent){
-            if (oEvent) {
-                
-            }
+
         },    
         onBeforeRebindTable: function(oEvent){
 
+        },
+        onBeforeExportExtension: function(oEvent){
+            if (oEvent) {
+                let mExcelSettings = oEvent.getParameter("exportSettings");
+                if (mExcelSettings.fileName === "Items")  {
+					mExcelSettings.fileName = "Reporte de Libro de compras IVA";
+				}
+                mExcelSettings.dataSource.sizeLimit = 250000;
+                mExcelSettings.dataSource.count = 250000;
+            }
+        },
+        onBeforeExport: function(oEvent){
+            if (oEvent) {
+                
+            }
         },
         onBeforeExcelExport: function(oEvent){
             if (oEvent) {
